4ff70755c44c4bfb0394e10482d32502ad2191e3
Louis Nathan Vessah Njoya Tchuente <vessahnjoyatchuentelouisnathan@gmail.com>
2026-01-04 19:10:51 +0100

    fix: black formatting + minor linting
    - Reformatted all scripts with black
    - Pre-commit check should pass

--- data-extraction-scripts/summary_generator.py
+++ data-extraction-scripts/summary_generator.py
@@ -1,10 +1,8 @@
-import yaml
-from pathlib import Path
 import pandas as pd
 from datetime import datetime
+
 
 class SummaryGenerator:
     """
     Generates a summary.md file for each training run, containing key hyperparameters from the config file
     """
-    
