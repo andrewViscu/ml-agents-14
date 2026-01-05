85f990519d2bfecbd7a41167af0bafd8f083a8c1
Louis Nathan Vessah Njoya Tchuente <vessahnjoyatchuentelouisnathan@gmail.com>
2026-01-05 19:34:35 +0100

    correct column name

--- PredictionModel/utils/data_loader.py
+++ PredictionModel/utils/data_loader.py
@@ -66,8 +66,8 @@ def extract_features(df, feature_cols=None):
             'step',
             'memory_usage_avg_mb',
             'memory_usage_peak_mb',
-            'cpu_usage_avg_mb',
-            'cpu_usage_peak_mb',
+            'cpu_usage_avg_percent',
+            'cpu_usage_peak_percent',
             'gpu_usage_avg_mb',
             'gpu_usage_peak_mb'
         ]
