d3bcc586a45743e1c395a11a7f2f6d04b0b08ace
Louis Nathan Vessah Njoya Tchuente <vessahnjoyatchuentelouisnathan@gmail.com>
2026-01-05 13:50:49 +0100

    update of data and correction of scripts, restructure of repo

--- data-extraction-scripts/summary_generator.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-from datetime import datetime
-
-
-class SummaryGenerator:
-    """
-    Generates a summary.md file for each training run, containing key hyperparameters from the config file
-    """
